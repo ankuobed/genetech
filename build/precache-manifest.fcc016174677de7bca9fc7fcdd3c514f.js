9cff4d207b0cffb368a91b745c9cc46df233e84c
Obed Anku <ankuobed@gmail.com>
2020-11-06 00:16:02 +0000

    removed the build folder from gitignore

---
+++ build/precache-manifest.fcc016174677de7bca9fc7fcdd3c514f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5bb7bb0f9fad75f0726888a4d08157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9356fac401baed0615d2",
+    "url": "/static/css/main.322f48cf.chunk.css"
+  },
+  {
+    "revision": "eb9499d2aabf0d7349b1",
+    "url": "/static/js/2.54950426.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.54950426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9356fac401baed0615d2",
+    "url": "/static/js/main.fd645c31.chunk.js"
+  },
+  {
+    "revision": "4288b37c182beeae940b",
+    "url": "/static/js/runtime-main.4fd4625c.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);

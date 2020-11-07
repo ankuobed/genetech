6f1284d0a38b4ee46c3e1211c15ff6e1b16943f0
Obed Anku <ankuobed@gmail.com>
2020-11-07 00:23:39 +0000

    connected backend

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,17 +2,35 @@ import React, { useState } from 'react'
 import './ContactForm.css'
 
 const ContactForm = () => {
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [message, setMessage] = useState()
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false);
 
+    //https://genetech-backend.herokuapp.com/contact
     const handleSubmit = (e) => {
         e.preventDefault()
+        setLoading(true);
+        fetch('https://genetech-backend.herokuapp.com/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, message })
+        }).then(res => {
+            setLoading(true);
+            if(!(res.status === 422)) {
+                return res.json().then(err => setError(err))
+            }
+          })
+          .catch(err => console.log(err));  
     }
 
     return (
         <form className='contactForm' onSubmit={handleSubmit}>
             <h1>Contact Us</h1>
+            {error&& <p className='contactForm__error'>{error}</p>}
             <label>Name <span>*</span></label>
             <input
                 type='text'

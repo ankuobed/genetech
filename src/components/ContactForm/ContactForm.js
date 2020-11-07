e55c99e146455ad95b6e366764788f3fa8e74aad
Obed Anku <ankuobed@gmail.com>
2020-11-07 10:35:16 +0000

    add progress to submit button

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import './ContactForm.css'
+import { CircularProgress } from '@material-ui/core'
 
 const ContactForm = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     //https://genetech-backend.herokuapp.com/contact
     const handleSubmit = (e) => {
         e.preventDefault()
-        fetch('https://genetech-backend.herokuapp.com/contact', {
+        setLoading(true)
+        fetch('http://localhost:5000/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ name, email, message })
         }).then(res => {
+            setLoading(false)
             if(res.status === 422) {
-                return res.json().then(err => setError(err))
+                res.json().then(err => setError(err))
             }
           })
           .catch(err => console.log(err));  
@@ -48,7 +52,7 @@ const ContactForm = () => {
                 onChange={(e) => setMessage(e.target.value)}
             />      
 
-            <button>SUBMIT</button>
+            <button>SUBMIT {loading&& <CircularProgress size={16} />}</button>
         </form>
     )
 }

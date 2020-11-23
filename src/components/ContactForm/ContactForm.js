ad29742f72fd9eab6c7fb9ccffe9ea647fc8cd3e
Obed Anku <ankuobed@gmail.com>
2020-11-23 13:51:37 +0000

    added a snackbar

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { CircularProgress, Input, TextField } from '@material-ui/core'
+import { CircularProgress, Input, TextField, Snackbar, SnackbarContent } from '@material-ui/core'
 import { Error, Check } from '@material-ui/icons'
 import axios from 'axios'
 
@@ -10,6 +10,7 @@ const ContactForm = () => {
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [error, setError] = useState(null)
+    const [networkError, setNetworkError] = useState('')
     const [success, setSuccess] = useState(null)
     const [loading, setLoading] = useState(false)
 
@@ -37,46 +38,69 @@ const ContactForm = () => {
                 if(err.response) {
                     setError(err.response.data)
                 } else {
-                    setError('Network too slow or inactive')
+                    setNetworkError('network too slow or inactive')
                 }
                 
             })
     }
 
     return (
-        <form className='contactForm' method='POST' onSubmit={handleSubmit}>
-            <h1>Contact Us</h1>
+        <div>
+            <form className='contactForm' method='POST' onSubmit={handleSubmit}>
+                <h1>Contact Us</h1>
 
-            <label>Name <span>*</span></label>
-            <Input
-                type='text'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                color='secondary'
-            />
+                <label>Name <span>*</span></label>
+                <Input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    color='secondary'
+                />
 
-            <label>Email <span>*</span></label>
-            <Input
-                type='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                color='secondary'
-            />
+                <label>Email <span>*</span></label>
+                <Input
+                    type='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    color='secondary'
+                />
 
-            <label>Message <span>*</span></label>
-            <TextField
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                multiline={true}
-                rows='10'
-                color='secondary'
-            />      
+                <label>Message <span>*</span></label>
+                <TextField
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    multiline={true}
+                    rows='10'
+                    color='secondary'
+                />      
 
-            {error&& <p className='contactForm__error'><Error />{error}</p>}
-            {success&& <p className='contactForm__success'><Check />{success}</p>}
+                {error&& <p className='contactForm__error'><Error />{error}</p>}
 
-            <button>{loading? <CircularProgress size={17} /> : 'SUBMIT'}</button>
-        </form>
+                <button>{loading? <CircularProgress size={17} /> : 'SUBMIT'}</button>
+            </form>
+
+            <Snackbar
+            open={success}
+            onClose={() => setSuccess('')}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center'
+            }}
+            autoHideDuration={3000}>
+                <p className='success'>{success}</p>
+            </Snackbar>
+
+            <Snackbar
+            open={networkError}
+            onClose={() => setNetworkError('')}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center'
+            }}
+            autoHideDuration={3000}>
+                <p className='netError'>{networkError}</p>
+            </Snackbar>
+        </div>
     )
 }
 
